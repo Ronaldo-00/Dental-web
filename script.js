39a2b7aad4b68a71440ecf6339c63c9196f8ea9e
Muthu Raman <110217804+Ronaldo-00@users.noreply.github.com>
2023-04-27 14:58:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+const mybutton = document.getElementById("gig");
+
+window.onscroll = function() {scrollings()};
+
+function scrollings() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
